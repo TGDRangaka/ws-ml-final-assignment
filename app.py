105de4e2da267dee414862ebb0ed3b4c7e769e25
TGDRangaka <dilshanrangaka@gmail.com>
2025-04-02 21:01:32 +0530

    add libraries

--- app.py
+++ app.py
@@ -3,12 +3,21 @@
 # python --version
 
 from flask import Flask
+import joblib
+import numpy as np
+import pandas as pd
+import warnings
+warnings.filterwarnings('ignore')
+
+# Load preprocessor and model
+preprocessor = joblib.load("preprocessor.pkl")
+svm_model = joblib.load("svm_model.pkl")
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<h1>AI v02</h1>'
+    return '<h1>AI v03</h1>'
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80) # to change the port .
